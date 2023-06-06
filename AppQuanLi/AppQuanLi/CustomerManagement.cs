96013a7fb852e48edf2e8b3b7782eb025b47e943
UIT.21521875 <21521875@gm.uit.edu.vn>
2023-06-06 19:02:52 +0700

    Tạo binding cho khách hàng

--- AppQuanLi/AppQuanLi/CustomerManagement.cs
+++ AppQuanLi/AppQuanLi/CustomerManagement.cs
@@ -18,6 +18,8 @@ namespace AppQuanLi
             InitializeComponent();
 
             loadCustomerList();
+
+            AddCustomerBinding();
         }
         void loadCustomerList()
         {
@@ -25,7 +27,7 @@ namespace AppQuanLi
 
 
             SqlConnection connection = new SqlConnection(connectionSTR);
-            string query = "SELECT MAKH, HOTEN, SODT, DIEMTICH\r\nFROM KHACHHANG INNER JOIN THONGTIN\r\nON KHACHHANG.MATT = THONGTIN.MATT";
+            string query = "SELECT KH.MAKH, KH.DIEMTICH, TT.HOTEN, TT.SODT, TT.DIACHI, TT.NGSINH, TT.CCCD, TT.EMAIL\r\nFROM KHACHHANG KH\r\nJOIN THONGTIN TT ON KH.MATT = TT.MATT;\r\n";
 
             connection.Open();
 
@@ -40,6 +42,17 @@ namespace AppQuanLi
             connection.Close();
 
             CustomerList.DataSource = data;
+        }
+        void AddCustomerBinding()
+        {
+            tb_hotenkh.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "HOTEN"));
+            tb_makh.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "MAKH"));
+            tb_ngaysinh.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "NGSINH"));
+            tb_sdt.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "SODT"));
+            tb_cccd.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "CCCD"));
+            tb_email.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "EMAIL"));
+            tb_diachi.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "DIACHI"));
+            tb_diem.DataBindings.Add(new Binding("Text", CustomerList.DataSource, "DIEMTICH"));
         }
 
         private void label5_Click(object sender, EventArgs e)
